b7ab559b7835a586b0b052a3c7ae0ccb25f0125b
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-03-08 12:57:19 +0700

    declear vaiables

---
+++ Variables/index.js
@@ -0,0 +1,22 @@
+let life = 100;  // this is number type variable
+console.log(life);
+
+life = 300; // we can change the value of variable but we can't change the type of variable
+
+// យើងអាចបង្ហាញតម្លៃនៃអថេរប្រភេទលេខដោយប្រើ console.log() ​បាន 3​របៀប ដូចខាងក្រោម៖
+console.log("Hello, my life is:", life);
+console.log("Hello, my life is:" + life);
+console.log(`Hello, my life is: ${life}`);
+
+
+character = "Mario"; // character type វាគឺជាប្រភេទអក្សរ ឬ string type variable 
+console.log(character);
+
+
+const pi = 3.14; // this is constant variable, we can't change the value of constant variable, អត់អាចប្ដូរតម្លៃបាន 
+console.log(pi);
+
+
+
+
+
