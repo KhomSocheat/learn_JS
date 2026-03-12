97cc6748311be45eedeb575d4a1d9893f0345676
Khom Socheat <khomsocheat@acledaaib.onmicrosoft.com>
2026-03-12 12:49:15 +0700

    Method Array Concat

---
+++ Array/concat.js
@@ -0,0 +1,8 @@
+const letter = ["a", "b", "c"];
+const number = [1, 2, 3];
+
+
+const combine = letter.concat(number); //! this method is used to combine two or more arrays and return a new array
+console.log(combine);
+console.log(letter); // ! this method does not change the original array
+console.log(number); // ! this method does not change the original array
